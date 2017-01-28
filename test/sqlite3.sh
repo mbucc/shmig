e90b9b94cf86026b09a323ac9608e473d19c4d96
Mark Bucciarelli <mkbucc@gmail.com>
2017-01-28 17:45:25 -0500

    Fix script comment

--- test/sqlite3.sh
+++ test/sqlite3.sh
@@ -1,4 +1,4 @@
 #! /bin/sh -e
-# Run SQL in migrations directory against SQLite3.
+# Run SQL in sqlite3 directory against SQLite3.
 
 docker run -v $(pwd)/sqlite3:/sql mkbucc/shmig -d test.db -t sqlite3 up
