d4d9862c71cdfe0ea483ec2777054b0c587ceaff
Mark Bucciarelli <mkbucc@gmail.com>
2017-03-01 09:56:46 -0500

    Fix and simplify regex

--- test/zapdate.sh
+++ test/zapdate.sh
@@ -1,4 +1,4 @@
 #! /bin/sh -e
-# Convert any dates in shmig output to current_time.
+# Convert any dates in shmig output to the string "*now*"
 
-sed 's/20[1-9][0-9]-[02][0-9]-[123][0-9] [012][0-9]:[0-5][0-9]:[0-5][0-9]\(\.[0-9]*\)*/current_time/'
+sed 's/20..-[012].-[0123]. ..:..:..\(\.[0-9]*\)*/*now*/'
